b04d4898d3f61b31e1b8e32f682a9efdff617244
marwa <marwa.hadidan@esprit.tn>
2021-04-13 00:35:31 +0200

    affichage

--- src/pijavafx/offre/AfficheoffreFXMLController.java
+++ src/pijavafx/offre/AfficheoffreFXMLController.java
@@ -5,65 +5,38 @@
  */
 package pijavafx.offre;
 
+
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
-
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import javafx.fxml.Initializable;
-import pijavafx.events.utils.BDConnector;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import pijavafx.events.utils.BDConnector;
 
 /**
  * FXML Controller class
  *
- * @author Lenovo
+ * @author fedi
  */
-
-public class offreFXMLController implements Initializable {
-
-        @FXML
-    private TextField t_id;
+public class AfficheoffreFXMLController implements Initializable {
 
     @FXML
-    private TextField t_specialite;
-
-    @FXML
-    private TextField t_localisation;
-
-    @FXML
-    private TextField t_description;
-
-    @FXML
-    private TextField t_imagesoffre;
-    
-
-    @FXML
-    private  TextField t_nb_dem;
-
- @FXML
     private TableView<Offre> table;
     @FXML
     private TableColumn<Offre, Integer> tab_id;
     @FXML
     private TableColumn<Offre, String> tab_specialite;
     @FXML
-    private TableColumn<Offre, String> tab_localisation ;
+    private TableColumn<Offre, String> tab_location;
     @FXML
     private TableColumn<Offre, Integer> tab_nb_dem;
     @FXML
@@ -76,11 +49,11 @@ public class offreFXMLController implements Initializable {
 
     /**
      * Initializes the controller class.
-     * @param url
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       try {
+        
+        try {
             Connection conx = BDConnector.driverBD();
             ResultSet rs = conx.createStatement().executeQuery("select * from offre");
             
@@ -97,33 +70,12 @@ public class offreFXMLController implements Initializable {
         }
          tab_id.setCellValueFactory(new PropertyValueFactory<>("id"));
                     tab_specialite.setCellValueFactory(new PropertyValueFactory<>("specialite"));
-                    tab_localisation.setCellValueFactory(new PropertyValueFactory<>("localisation"));
+                    tab_location.setCellValueFactory(new PropertyValueFactory<>("localisation"));
                     tab_nb_dem.setCellValueFactory(new PropertyValueFactory<>("nb_dem"));
                     tab_description.setCellValueFactory(new PropertyValueFactory<>("desception"));
                     tab_imagesoffre.setCellValueFactory(new PropertyValueFactory<>("imagesoffre"));
                     
                     table.setItems(oblist);
     }    
-     @FXML
-    private void hundleaddoffre(ActionEvent event) throws SQLException {
-       
-         int id = Integer.parseInt(t_id.getText());
-       
-        String specialite = t_specialite.getText();
-        String localisation  = t_localisation.getText(); 
-       int nb_dem = Integer.parseInt(t_nb_dem.getText());
-        String description = t_description.getText();
-        String imagesoffre = t_imagesoffre.getText();
-     
-        
-       
-          Offre f = new Offre (id, specialite , localisation  ,nb_dem , description  , imagesoffre);      
-        offreCrud fc = new offreCrud();
-        fc.addoffre(f);
-               
-        
-       
-            
-        
-    }
+    
 }
